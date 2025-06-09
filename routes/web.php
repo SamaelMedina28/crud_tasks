72fb8e5acc696d366c2358c2cd0a601d612e777b
Samael Home <samaelortiz2218@gmail.com>
2025-06-09 08:55:27 -0700

    Correccion en error en vista welcome

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('inicio');
+    return view('welcome');
 });
 Route::resource('tasks',TaskController::class)->parameters(['tasks'=>'task'])->names('tasks');
